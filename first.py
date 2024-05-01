ab1469521c057f6d0d2b8fc93b98bd7abe210512
Rinkoo <rinkoo.bahrani@gmail.com>
2024-05-01 14:13:03 +1200

    CRUD done

--- first.py
+++ first.py
@@ -3,15 +3,15 @@ import sqlite3
 connection = sqlite3.connect("store.db")
 cursor = connection.cursor()
 
-command1 = """"CREATE TABLE IF NOT EXISTS
+command1 = """CREATE TABLE IF NOT EXISTS
       stores (store_id INTEGER PRIMARY KEY, location TEXT)"""
 
 cursor.execute(command1)
 
+#CREATE 
 command2 = """CREATE TABLE IF NOT EXISTS
 purchases(purchase_id INTEGER PRIMARY KEY, store_id INTEGER, total_cost FLOAT, 
-FOREIGN KEY (store_id) 
-REFERENCES stores(store_id))"""
+FOREIGN KEY (store_id) REFERENCES stores(store_id))"""
 cursor.execute(command2)
 
 cursor.execute("INSERT INTO stores VALUES (21, 'Howick')")
@@ -23,6 +23,19 @@ cursor.execute("INSERT INTO purchases VALUES (2, 86, 15.11)")
                
 cursor.execute("SELECT * FROM purchases ")
 
+results = cursor.fetchall()
+print(results)
+
+
+#KITE AI coding assistance
+#UPDATE
+cursor.execute("UPDATE purchases SET total_cost = 3.67 WHERE purchase_id = 1")
+
+cursor.execute("SELECT * FROM purchases ")
 
 results = cursor.fetchall()
 print(results)
+
+#Delete
+
+cursor.execute("DELETE FROM purchases WHERE purchase id =1")
